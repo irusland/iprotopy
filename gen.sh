f4ac169a2b0625375a5c275538bd40595bbeb402
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-07-21 19:32:14 +0300

    fix

--- gen.sh
+++ gen.sh
@@ -6,13 +6,13 @@ PROTO_INCLUDE_PATH=$PROJECT_ROOT/tinkoff/invest/grpc
 GOOGLE_API_PATH=$PROJECT_ROOT/tinkoff/invest/grpc/google/api
 
 # Ensure the protoc-gen-python_betterproto plugin is available in PATH
-export PATH=$PATH:$(python -m site --user-base)/bin
+#export PATH=$PATH:$(python -m site --user-base)/bin
 
 # Run the protoc command with the correct paths
 python -m grpc_tools.protoc --proto_path=$PROJECT_ROOT \
     --proto_path=$PROTO_INCLUDE_PATH \
     --proto_path=$GOOGLE_API_PATH \
-    --python_betterproto_out=$PROJECT_ROOT \
+    --python_betterproto_out=$PROJECT_ROOT/models \
     $PROTO_INCLUDE_PATH/instruments.proto 2> error.log
 
 # Check if the generation was successful
