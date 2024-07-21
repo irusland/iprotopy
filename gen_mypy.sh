36e8a42567e7de04f4fa34dd690a485745e1314d
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-07-21 20:22:02 +0300

    mypy

---
+++ gen_mypy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PROJECT_ROOT=$(pwd)
+PROTO_INCLUDE_PATH=$PROJECT_ROOT/tinkoff/invest/grpc
+GOOGLE_API_PATH=$PROJECT_ROOT/tinkoff/invest/grpc/google/api
+MODELS_PATH=$PROJECT_ROOT/mypy_models
+
+mkdir $MODELS_PATH
+
+python -m grpc_tools.protoc \
+	--proto_path=$PROJECT_ROOT \
+    --mypy_out=$MODELS_PATH \
+    --python_out=$MODELS_PATH \
+    $PROTO_INCLUDE_PATH/*.proto
