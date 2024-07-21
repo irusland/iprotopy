299f7c8b12c98b331e5d5112fbe171505d9d5778
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-07-21 20:09:12 +0300

    better proto gen all

--- gen_betterproto.sh
+++ gen_betterproto.sh
@@ -3,9 +3,10 @@
 PROJECT_ROOT=$(pwd)
 PROTO_INCLUDE_PATH=$PROJECT_ROOT/tinkoff/invest/grpc
 GOOGLE_API_PATH=$PROJECT_ROOT/tinkoff/invest/grpc/google/api
+MODELS_PATH=$PROJECT_ROOT/betterproto_models
 
-mkdir models
+mkdir $MODELS_PATH
 
 python -m grpc_tools.protoc --proto_path=$PROJECT_ROOT \
-    --python_betterproto_out=$PROJECT_ROOT/models \
-    $PROTO_INCLUDE_PATH/instruments.proto
+    --python_betterproto_out=$MODELS_PATH \
+    $PROTO_INCLUDE_PATH/*.proto
