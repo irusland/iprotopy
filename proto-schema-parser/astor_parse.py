51298bc74e846be511252f7e302875df81d37096
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-08-12 23:59:02 +0300

    ast

---
+++ proto-schema-parser/astor_parse.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+
+import astor
+import ast
+from ast import *
+
+src = """
+from typing import Optional
+
+
+class ClassName:
+    field1: int
+    field2: Optional[bool] = False
+    
+"""
+
+ast_src = ast.parse(src)
+
+print(ast.dump(ast_src))
+
+Module(
+    body=[
+        ImportFrom(module='typing', names=[alias(name='Optional')], level=0),
+          ClassDef(
+              name='ClassName', bases=[], keywords=[], body=[
+                  AnnAssign(
+                    target=Name(id='field1', ctx=Store()),
+                    annotation=Name(id='int', ctx=Load()), simple=1
+                  ),
+                  AnnAssign(
+                  target=Name(id='field2', ctx=Store()), annotation=Subscript(
+                      value=Name(id='Optional', ctx=Load()),
+                      slice=Name(id='bool', ctx=Load()), ctx=Load()
+                  ), value=Constant(value=False), simple=1
+              )], decorator_list=[]
+          )], type_ignores=[]
+)
