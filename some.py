c399270b59bbbca859e33b6465aead5ae7b16bec
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-10-05 15:46:46 +0300

    rm excess

--- some.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class PersonModel(BaseModel):
-    name: str
-    age: int
-
-class PersonResponseModel(BaseModel):
-    message: str
--- use.py
+++
@@ -1,6 +0,0 @@
-from example_pb2 import Person
-
-p = Person(name='John Doe', age=25)
-p.name = 123
-
-
