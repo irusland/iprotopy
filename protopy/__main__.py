60c43950cdb40ff29cc67ee566b0bcf80fdd7180
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-10-08 22:44:21 +0300

    add

---
+++ protopy/__main__.py
@@ -0,0 +1,14 @@
+import logging
+
+from paths import ROOT_DIR
+from protopy.package_generator import PackageGenerator
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    gen = PackageGenerator()
+    usage_dir = ROOT_DIR / 'usage'
+    gen.generate_sources(
+        proto_dir=usage_dir / 'protos',
+        out_dir=usage_dir / 'package',
+    )
